3f92d01c646bc3ba38b149f38ea6dfff593b126c
SashaDE1 <sasharodce17@gmail.com>
2026-07-14 15:11:11 +0300

    add docker-compose and change settings

--- config/settings.py
+++ config/settings.py
@@ -20,3 +20,12 @@ SILVER_STATUS_PATH="silver/status"
 SILVER_STATIONS_PATH="silver/stations"
 SILVER_WEATHER_PATH="silver/weather"
 SILVER_TRIPS_PATH="silver/trips"
+
+#--POSTGRES--
+DB_USER=os.getenv("DB_USER", "logi_admin")
+DB_PASSWORD=os.getenv("DET_PASSWORD", "logi_secure_pass")
+DB_NAME=os.getenv("DB_NAME", "logi_db")
+DB_PORT=os.getenv("DB_PORT", "5432")
+DB_HOST="localhost"
+
+DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
